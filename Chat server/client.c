04162551ba130802bfc65f838ac2dee308920d6b
Esha Khadeeja C <119195256+EshaKhadeejaC@users.noreply.github.com>
2024-10-19 11:04:25 +0530

    Update client.c

--- Chat server/client.c
+++ Chat server/client.c
@@ -1,30 +1,59 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <sys/socket.h>
+#include <unistd.h>
 #include <arpa/inet.h>
-#include <netinet/in.h>
+/*The above imports are actually enough in latest C distributions as all other necessary modules
+are linked within arpa/inet.h . But chances are if you are using an older distribution you might
+have to include these additional modules too*/
+#include<sys/socket.h>
+#include<sys/types.h>
+#include<netinet/in.h>
+#include<netdb.h>
 
-int main(){
-	int clientSocket;
-	char buffer[1024];
-	struct sockaddr_in serverAddr;
-	socklen_t addrSize;
-	
-	clientSocket = socket(AF_INET,SOCK_STREAM,0);
-	
-	serverAddr.sin_family = AF_INET;
-	serverAddr.sin_port = htons(9999);
-	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-	
-	memset(serverAddr.sin_zero,'\0',sizeof(serverAddr.sin_zero));
-	
-	addrSize = sizeof(serverAddr);
-	
-	connect(clientSocket,(struct sockaddr *)&serverAddr,addrSize);
+#define PORT 8090
+#define BUFFER_SIZE 1024
+#define IP "127.0.0.1"
 
-	while(1){
-		printf("Enter the msg to send: ");
-		fgets(buffer,sizeof(buffer),stdin);
-		send(clientSocket,buffer,sizeof(buffer),0);
-	}
+int main() {
+    int sockfd;
+    struct sockaddr_in address;
+    char buffer[BUFFER_SIZE] = {0};
+
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        perror("socket");
+        exit(1);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_port = htons(PORT);
+    if (inet_pton(AF_INET, IP, &address.sin_addr) <= 0) {
+        perror("invalid address");
+        close(sockfd);
+        exit(1);
+    }
+
+    if (connect(sockfd, (struct sockaddr *)&address, sizeof(address)) == -1) {
+        perror("connect");
+        close(sockfd);
+        exit(1);
+    }
+
+    char name[BUFFER_SIZE];
+    printf("\nEnter the name to be displayed in chat server: ");
+    fgets(name, BUFFER_SIZE, stdin);
+    name[strcspn(name, "\n")] = 0;
+
+    send(sockfd, name, strlen(name), 0); 
+    printf("\nConnected to the chat server as %s\n", name);
+
+    while (1) {
+        printf("\nEnter message to send: ");
+        fgets(buffer, BUFFER_SIZE, stdin);
+        buffer[strcspn(buffer, "\n")] = 0;
+        send(sockfd, buffer, strlen(buffer), 0);
+    }
+
+    close(sockfd);
+    return 0;
 }
