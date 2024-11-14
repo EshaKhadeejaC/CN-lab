39f435a3a2502c181565881be1d2206666e7689a
Esha Khadeeja C <119195256+EshaKhadeejaC@users.noreply.github.com>
2024-11-14 22:28:56 +0530

    Update server.c

--- SWP/gobackN/server.c
+++ SWP/gobackN/server.c
@@ -4,44 +4,57 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <sys/select.h>
 
 #define PORT 9009
-#define SERVER_IP "127.0.0.1"
+#define TIMEOUT_SEC 2
+#define TOTAL_MSGS 10
+#define GO_BACK_N 3
 
 int main() {
-    int client_sock;
-    struct sockaddr_in server_addr;
-    char buffer[100];
-    char ack_msg[50];
+    int server_sock, client_sock;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t addr_size;
+    fd_set read_fds;
+    struct timeval timeout;
+    char msg[50];
+    char buffer[50];
+    int msg_num = 0;
 
-    client_sock = socket(AF_INET, SOCK_STREAM, 0);
-    memset(&server_addr, 0, sizeof(server_addr));
+    server_sock = socket(AF_INET, SOCK_STREAM, 0);
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(PORT);
-    server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    bind(server_sock, (struct sockaddr*)&server_addr, sizeof(server_addr));
 
-    if (connect(client_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
-        printf("Connection failed\n");
-        return 0;
-    }
+    printf("\tServer Up\nGo back n (n=3) used to send 10 messages\n\n");
 
-    printf("\nClient - with individual acknowledgement scheme\n\n");
+    listen(server_sock, 10);
+    addr_size = sizeof(client_addr);
+    client_sock = accept(server_sock, (struct sockaddr*)&client_addr, &addr_size);
 
-    for (int i = 0; i < 10; i++) {
-        memset(buffer, 0, sizeof(buffer));
-        int n = read(client_sock, buffer, sizeof(buffer));
+    while (msg_num < TOTAL_MSGS) {
+        snprintf(msg, sizeof(msg), "server message: %d", msg_num);
+        printf("Message sent to client: %s\n", msg);
+        send(client_sock, msg, strlen(msg) + 1, 0);
 
-        if (buffer[strlen(buffer) - 1] == i + '0') {
-            printf("Message received from server: %s\n", buffer);
-            snprintf(ack_msg, sizeof(ack_msg), "acknowledgement of : %d", i);
-            printf("Acknowledgement sent for message\n");
-            write(client_sock, ack_msg, strlen(ack_msg) + 1);
+        FD_ZERO(&read_fds);
+        FD_SET(client_sock, &read_fds);
+        timeout.tv_sec = TIMEOUT_SEC;
+        timeout.tv_usec = 0;
+
+        if (select(client_sock + 1, &read_fds, NULL, NULL, &timeout) > 0) {
+            recv(client_sock, buffer, sizeof(buffer), 0);
+            printf("Message from Client: %s\n", buffer);
+            msg_num++;
         } else {
-            printf("Discarded as out of order\n");
-            i--;
+            printf("Timeout! Going back from %d\n", msg_num);
+            msg_num = (msg_num - GO_BACK_N >= 0) ? msg_num - GO_BACK_N : 0;
         }
     }
 
     close(client_sock);
+    close(server_sock);
     return 0;
 }
+
